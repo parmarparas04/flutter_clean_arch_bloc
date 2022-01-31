c4cafa376fce762d65de51ded029b9aeaf74a540
paras <riakachhawaha@Rias-MacBook-Air.local>
2022-01-31 18:54:36 +0530

    first commit flutter clean architure with bloc pattern

---
+++ lib/data/repository/repository_impl.dart
@@ -0,0 +1,51 @@
+import 'package:dartz/dartz.dart';
+import 'package:first_app/data/data_source/remote_data_source.dart';
+import 'package:first_app/data/mapper/mapper.dart';
+import 'package:first_app/data/network/error_handler.dart';
+import 'package:first_app/data/network/failure.dart';
+import 'package:first_app/data/network/network_info.dart';
+import 'package:first_app/data/request/request.dart';
+import 'package:first_app/domain/model/model.dart';
+import 'package:first_app/domain/repository/repository.dart';
+
+class RepositoryImpl extends Repository {
+  RemoteDataSource _remoteDataSource;
+  NetworkInfo _networkInfo;
+
+  RepositoryImpl(this._remoteDataSource, this._networkInfo);
+
+  @override
+  Future<Either<Failure, Authentication>> login(
+      LoginRequest loginRequest) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        print("LoginRequest response");
+
+        // its safe to call the API
+        final response = await _remoteDataSource.login(loginRequest);
+        print("LoginRequest response2");
+
+        if (response.status == ApiInternalStatus.SUCCESS) // success
+        {
+          print("LoginRequest success");
+          // return data (success)
+          // return right
+          return Right(response.toDomain());
+        } else {
+          print("LoginRequest failur");
+          // return biz logic error
+          // return left
+
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        print("LoginRequestsadasd error ${error}");
+        return (Left(ErrorHandler.handle(error).failure));
+      }
+    } else {
+      // return connection error
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
+    }
+  }
+}
